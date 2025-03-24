b43aa39f9db40bc8e4c7fea87386a5c4bba404a9
kulekule2003 <okureenock@gmail.com>
2025-03-24 11:02:41 +0300

    added cookies for authentication

--- BACKEND/base/views.py
+++ BACKEND/base/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .models import Note
+from .serializers import NoteSerializer
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        try:
+            response = super().post(request, *args, **kwargs)
+            tokens = response.data
+
+            access_token = tokens['access']
+            refresh_token = tokens['refresh']
+
+            res = Response()
+
+            res.data = {'success':True}
+
+            res.set_cookie(
+                key='access_token',
+                value=access_token,
+                httponly=True,
+                secure=True,
+                samesite='None',
+                path='/'
+            )
+
+            res.set_cookie(
+                key='refresh_token',
+                value=refresh_token,
+                httponly=True,
+                secure=True,
+                samesite='None',
+                path='/'
+            )
+
+            return res
+
+        except:
+            return Response({'success':False})
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_notes(request):
+    user = request.user
+    notes = Note.objects.filter(owner=user)
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
